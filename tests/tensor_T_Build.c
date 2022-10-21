f06e6296e612f688f84246b143078cc4bdb98a65
Riccardo Sven Risuleo <riccardosven@gmail.com>
2022-10-21 16:30:28 +0200

    Added test for direct tensor builder

---
+++ tests/tensor_T_Build.c
@@ -0,0 +1,25 @@
+#include "common.h"
+#include "storch/storch.h"
+#include "storch/tensor.h"
+#include <stdlib.h>
+
+int
+main(void)
+{
+
+  Tensor* t = T_Build(NULL, 3, 6, 1.0, 2.0, -1.3, 2.2, 4.0, 5.1);
+
+  T_eltype e[] = {1.0, 2.0, -1.3, 2.2, 4.0, 5.1};
+
+  int retval = 0;
+
+
+  for (int i=0; i<6; i++) {
+      printf("%f == %f ?\n", t->data[i], e[i]);
+      retval += check_almost_eq(t->data[i], e[i]);
+  }
+
+  T_Destroy(t);
+
+  return retval ? EXIT_FAILURE : EXIT_SUCCESS ;
+}
